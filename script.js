255fd51c53367cd595ae72cd6d822a48ab1f1174
Jayden Yang <jayden.yang@rea-group.com>
2025-05-03 10:23:33 +1000

    Implement the new rule of the interest

--- script.js
+++ script.js
@@ -99,6 +99,7 @@ const calDisplaySummary = function (movements) {
   const interest = movements
     .filter(mov => mov > 0)
     .map(deposit => deposit * 1.2 / 100)
+    .filter(int => int >= 1)
     .reduce((acc, int) => acc + int, 0);
   labelSumInterest.textContent = `${interest}€`;
 }
